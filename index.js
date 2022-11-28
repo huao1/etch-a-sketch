b645847af3b43e1b12815ccba1b46ec42ac93fc9
huao1 <huao.cheng@gmail.com>
2022-11-28 17:57:01 -0500

    Added a slider in HTML and CSS that allows the user to select the size of the grid for the sketch pad. Also included JavaScript to display the value of the slider to show the size of the grid the user will be creating. The slider works but the feature to update the size of the sketch pad is not completed.

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 
-function createGrid() {
-    const sketchContainer = document.querySelector("div");
+function sliderDisplay() {
+    const slider = document.querySelector("#myRange");
+    const value = document.querySelector("#slider-value");
 
-    for (let i = 0; i < 16; i++) {
+    value.textContent = `${slider.value}x${slider.value}`;
+
+    slider.oninput = function () {
+        console.log(slider.value);
+        value.textContent = `${this.value}x${this.value}`;
+    }
+}
+
+function createGrid(size = 16) {
+    const sketchContainer = document.querySelector(".sketch-container");
+
+    //let width = 960/17;
+    //width = Math.round(width * 100) / 100;
+    //console.log(width);
+
+    for (let i = 0; i < size; i++) {
         const divRow = document.createElement("div");
         sketchContainer.appendChild(divRow);
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             const sketchGrid = document.createElement("div");
             sketchGrid.classList.add("sketch-grid");
             sketchGrid.classList.add("sketch-grid:hover");
@@ -17,3 +33,4 @@ function createGrid() {
 }
 
 createGrid();
+sliderDisplay();
