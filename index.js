36a710de9fa871c66877bfca5e3d374bc56fe92d
huao1 <huao.cheng@gmail.com>
2022-11-30 10:50:07 -0500

    Added buttons for the modes rainbow and eraser. The buttons css will change depending on which mode is selected. Rainbow mode randomizes the color when drawing.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 function sliderDisplay() {
     const slider = document.querySelector("#myRange");
     const value = document.querySelector("#slider-value");
@@ -15,8 +19,6 @@ function createGrid(size = 16) {
     const sketchContainer = document.querySelector(".grid-container");
 
     let totalWidth = 560;
-    //width = Math.round(width * 100) / 100;
-    //console.log(width);
 
     for (let i = 0; i < size; i++) {
         const divRow = document.createElement("div");
@@ -33,7 +35,7 @@ function createGrid(size = 16) {
 }
 
 function submitGridSize() {
-    const btn = document.querySelector("button");
+    const btn = document.querySelector("#submit");
     btn.addEventListener("click", updateGrid);
 }
 
@@ -49,6 +51,72 @@ function updateGrid() {
     createGrid(size.value);
 }
 
+function randomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+
+    return `rgb(${red}, ${blue}, ${green});`;
+
+}
+
+function toggleMode() {
+    const rainbow = document.querySelector("#rainbow");
+    rainbow.addEventListener("click", rainbowMode);
+
+    const eraser = document.querySelector("#eraser");
+    eraser.addEventListener("click", eraserMode);
+
+    console.log(rainbow);
+    console.log(eraser);
+}
+
+function rainbowMode(e) {
+    const grids = document.querySelectorAll(".grid");
+
+    const buttons = document.querySelectorAll(".setting-buttons");
+    for (const button of buttons) {
+        button.classList.remove("on");
+        button.classList.add("off");
+    }
+    e.target.classList.add("on");
+
+    grids.forEach((grid) => {
+        grid.addEventListener("mousedown", () => {
+            grid.style.cssText += `background-color: ${randomColor()}`;
+        });
+        grid.addEventListener("mouseenter", () => {
+            if (mouseDown == true) {
+                grid.style.cssText += `background-color: ${randomColor()}`;
+            }
+        });
+    });
+}
+
+function eraserMode(e) {
+
+    const buttons = document.querySelectorAll(".setting-buttons");
+    for (const button of buttons) {
+        button.classList.remove("on");
+        button.classList.add("off");
+    }
+    e.target.classList.add("on");
+
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid) => {
+        grid.addEventListener("mousedown", () => {
+            grid.style.cssText += "background-color: white";
+        });
+        grid.addEventListener("mouseenter", () => {
+            if (mouseDown == true) {
+                grid.style.cssText += "background-color: white";
+            }
+        });
+    });
+}
+
 createGrid();
 sliderDisplay();
 submitGridSize();
+console.log(randomColor());
+toggleMode();
